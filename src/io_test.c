4a3f07636750e4250f735387ec0edd58d5c36f67
tomoya-yokota <tomoya-yokota@cybozu.co.jp>
2018-06-06 12:29:14 +0900

    DAY-8 テストケースを増やした

--- src/io_test.c
+++ src/io_test.c
@@ -5,40 +5,65 @@
 
 int tests_run = 0; 
 
-static char* test_dec_to_ascii_zero() {
-    struct Padding_Info padding_info;
-    padding_info.length = 3;
-    padding_info.padding = ' ';
-    char str[30];
-    int ret = dec_to_ascii(str, 0, padding_info);
+struct dec_to_ascii_expected {
+    char* string;
+    int length;
+};
 
-    mu_assert("method should return length of read characters", ret == 3);
+struct dec_to_ascii_input {
+    int dec;
+    int padding_length;
+    char padding_char;
+};
+
+static char* test_dec_to_ascii(struct dec_to_ascii_input input, struct dec_to_ascii_expected expected) {
+    struct Padding_Info padding_info;
+    padding_info.length = input.padding_length;
+    padding_info.padding = input.padding_char;
+
+    char str[256];
+    int ret = dec_to_ascii(str, input.dec, padding_info);
+
+    mu_assert("method should return length of read characters", ret == expected.length);
     
-    char err[30];
-    printf("expect str filled with `   0` , actual:`%s`\n", str);
-    mu_assert("str format error", strcmp(str, "  0") == 0);
+    printf("expect str filled with `%s` , actual:`%s`\n", expected.string, str);
+    mu_assert("str format error", strcmp(str, expected.string) == 0);
     return 0;
 }
 
-static char* test_dec_to_ascii_minus() {
-    struct Padding_Info padding_info;
-    padding_info.length = 3;
-    padding_info.padding = ' ';
-    char str[30];
-    int ret = dec_to_ascii(str, -10, padding_info);
+#define DEC_TO_ASCII_TEST_CASE 7
+static char* test_dec_to_ascii_parameterized() {
+    struct dec_to_ascii_input test_input[DEC_TO_ASCII_TEST_CASE] = {
+        {10, 3, ' '},
+        {-10, 3, ' '},
+        {1, 3, ' '},
+        {-1, 3, ' '},
+        {0, 3, ' '},
+        {1234, 3, ' '},
+        {-1234, 3, ' '},
+    };
 
-    mu_assert("method should return length of read characters", ret == 3);
-    
-    char err[30];
-    printf("expect str filled with `-10` , actual:`%s`\n", str);
-    mu_assert("str format error", strcmp(str, "-10") == 0);
+    struct dec_to_ascii_expected test_expected[DEC_TO_ASCII_TEST_CASE] = {
+        {" 10", 3},
+        {"-10", 3},
+        {"  1", 3},
+        {" -1", 3},
+        {"  0", 3},
+        {"1234", 4},
+        {"-1234", 5},
+    };
+
+    for(int i = 0; i < DEC_TO_ASCII_TEST_CASE; i++) {
+         struct dec_to_ascii_input input = test_input[i];
+         struct dec_to_ascii_expected expected = test_expected[i];
+         test_dec_to_ascii(input, expected);
+    }
     return 0;
 }
 
 
 static char * all_tests() {
-     mu_run_test(test_dec_to_ascii_zero);
-     mu_run_test(test_dec_to_ascii_minus);
+     mu_run_test(test_dec_to_ascii_parameterized);
      return 0;
 }
 
