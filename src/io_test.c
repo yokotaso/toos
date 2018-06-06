21091329018268204041b0a1ed329b8d065a1b28
tomoya-yokota <tomoya-yokota@cybozu.co.jp>
2018-06-06 09:04:31 +0900

    DAY-8 テストコードを追加

--- src/io_test.c
+++ src/io_test.c
@@ -5,7 +5,7 @@
 
 int tests_run = 0; 
 
-static char* test_dec_to_ascii() {
+static char* test_dec_to_ascii_zero() {
     struct Padding_Info padding_info;
     padding_info.length = 3;
     padding_info.padding = ' ';
@@ -20,8 +20,25 @@ static char* test_dec_to_ascii() {
     return 0;
 }
 
+static char* test_dec_to_ascii_minus() {
+    struct Padding_Info padding_info;
+    padding_info.length = 3;
+    padding_info.padding = ' ';
+    char str[30];
+    int ret = dec_to_ascii(str, -10, padding_info);
+
+    mu_assert("method should return length of read characters", ret == 3);
+    
+    char err[30];
+    printf("expect str filled with `-10` , actual:`%s`\n", str);
+    mu_assert("str format error", strcmp(str, "-10") == 0);
+    return 0;
+}
+
+
 static char * all_tests() {
-     mu_run_test(test_dec_to_ascii);
+     mu_run_test(test_dec_to_ascii_zero);
+     mu_run_test(test_dec_to_ascii_minus);
      return 0;
 }
 
